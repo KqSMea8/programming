bb3fcd2539615dd24dcd461ef58017f2bea88d9c
houjie <leeweiiii@163.com>
2018-04-25 17:40:58 +0800

    冒泡排序

---
+++ bubble_sort.py
@@ -0,0 +1,59 @@
+# -*- coding: utf8 -*-
+# the best
+def bubble_sort_2(lst):
+    lstLen = len(lst)
+    #count = 0
+    while lstLen >= 2:
+        unorder_len = 1 # 剩余未排序长度
+        for i in xrange(1, lstLen):
+            #count += 1
+            if lst[i] < lst[i-1]:
+                lst[i],lst[i-1] = lst[i-1],lst[i]
+                unorder_len = i
+                
+        lstLen = unorder_len
+
+    #print "compare:",count
+
+#the better
+def bubble_sort_1(lst):
+    lstLen = len(lst)
+    #count = 0
+    Flag = False
+    while lstLen >= 2:
+        for i in xrange(1, lstLen):
+            #count += 1
+            if lst[i] < lst[i-1]:
+                lst[i],lst[i-1] = lst[i-1],lst[i]
+                Flag = True
+                
+        if Flag:
+            Flag = False
+        else:
+            break
+        
+        lstLen -= 1
+
+    #print "compare:",count
+
+def bubble_sort(lst):
+    lstLen = len(lst)
+    #count = 0
+    while lstLen >= 2:
+        for i in xrange(1, lstLen):
+            #count += 1
+            if lst[i] < lst[i-1]:
+                lst[i],lst[i-1] = lst[i-1],lst[i]
+                
+        lstLen -= 1
+
+    #print "compare:",count
+
+def isSorted(lst, ascend = True):
+    lstLen = len(lst)
+    for i in xrange(1,lstLen):
+        if ascend and lst[i-1] > lst[i]:
+            return False
+        if (not ascend) and lst[i-1] < lst[i]:
+            return False
+    return True
