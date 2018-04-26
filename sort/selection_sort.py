4ee0c778ce81614b57f625259ff38768608eec9a
houjie <leeweiiii@163.com>
2018-04-26 17:46:24 +0800

    选择排序

---
+++ sort/selection_sort.py
@@ -0,0 +1,12 @@
+def selection_sort(l):
+    l_len = len(l)
+    for i in xrange(l_len - 1):
+        min_index = i
+        for j in xrange(i+1, l_len):
+	    if l[min_index] > l[j]:
+	        min_index = j
+
+        if min_index != i:
+	    temp = l[min_index]
+	    l[min_index] = l[i]
+	    l[i] = temp
