64155608076fbaec30e2d080f5cc0bdf43067df1
houj <leeweiiii@163.com>
2018-09-11 16:09:38 +0800

    windows服务程序开发

---
+++ util/windows_service_template.cpp
@@ -0,0 +1,160 @@
+﻿#include <Windows.h>
+#include <tchar.h>
+#include <string>
+#include <fstream>
+
+
+SERVICE_STATUS        g_ServiceStatus = { 0 };
+SERVICE_STATUS_HANDLE g_StatusHandle = NULL;
+HANDLE                g_ServiceStopEvent = INVALID_HANDLE_VALUE;
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv);
+VOID WINAPI ServiceCtrlHandler(DWORD);
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam);
+
+#define SERVICE_NAME  ("My Sample Service")
+
+void write_txt_file(std::string file_name, std::string input) {
+	/*
+	write a string to a specific txt file
+	*/
+	std::ofstream file(file_name, std::ios::app);
+	file << input;
+	file.close();
+}
+
+
+int _tmain(int argc, TCHAR *argv[])
+{
+	SERVICE_TABLE_ENTRY ServiceTable[] =
+	{
+		{ (char *)SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+	    { NULL, NULL }
+	};
+
+	if (StartServiceCtrlDispatcher(ServiceTable) == FALSE)
+	{
+		return GetLastError();
+	}
+
+	return 0;
+}
+
+
+VOID WINAPI ServiceMain(DWORD argc, LPTSTR *argv)
+{
+	HANDLE hThread;
+
+	g_StatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
+
+	if (g_StatusHandle == NULL)
+	{
+		return;
+	}
+
+	// Tell the service controller we are starting
+	ZeroMemory(&g_ServiceStatus, sizeof(g_ServiceStatus));
+	g_ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+	/*
+	* Perform tasks neccesary to start the service here
+	*/
+
+	// Create stop event to wait on later.
+	g_ServiceStopEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (g_ServiceStopEvent == NULL)
+	{
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+		g_ServiceStatus.dwWin32ExitCode = GetLastError();
+		g_ServiceStatus.dwCheckPoint = 1;
+
+		SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+		return;
+	}
+
+	// Tell the service controller we are started
+	g_ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+	g_ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 0;
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+	// Start the thread that will perform the main task of the service
+	hThread = CreateThread(NULL, 0, ServiceWorkerThread, NULL, 0, NULL);
+
+	// Wait until our worker thread exits effectively signaling that the service needs to stop
+	WaitForSingleObject(hThread, INFINITE);
+
+
+	/*
+	* Perform any cleanup tasks
+	*/
+
+	CloseHandle(g_ServiceStopEvent);
+
+	g_ServiceStatus.dwControlsAccepted = 0;
+	g_ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+	g_ServiceStatus.dwWin32ExitCode = 0;
+	g_ServiceStatus.dwCheckPoint = 3;
+
+	SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+	return;
+}
+
+
+VOID WINAPI ServiceCtrlHandler(DWORD CtrlCode)
+{
+	switch (CtrlCode)
+	{
+	case SERVICE_CONTROL_STOP:
+
+		if (g_ServiceStatus.dwCurrentState != SERVICE_RUNNING)
+			break;
+
+		/*
+		* Perform tasks neccesary to stop the service here
+		*/
+
+		g_ServiceStatus.dwControlsAccepted = 0;
+		g_ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+		g_ServiceStatus.dwWin32ExitCode = 0;
+		g_ServiceStatus.dwCheckPoint = 4;
+
+		SetServiceStatus(g_StatusHandle, &g_ServiceStatus);
+
+		// This will signal the worker thread to start shutting down
+		SetEvent(g_ServiceStopEvent);
+
+		break;
+
+	default:
+		break;
+	}
+}
+
+
+DWORD WINAPI ServiceWorkerThread(LPVOID lpParam)
+{
+	int i = 0;
+	//  Periodically check if the service has been requested to stop
+	while (WaitForSingleObject(g_ServiceStopEvent, 0) != WAIT_OBJECT_0)
+	{
+		/*
+		* Perform main service function here
+		*/
+
+		write_txt_file("d:\\out.txt", "Writing...#" + std::to_string(i) + "\n");
+		Sleep(1000);
+		i++;
+	}
+	return ERROR_SUCCESS;
+}
