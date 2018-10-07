426209b7b60d88f8820f843f050a29afe9bcc766
houj <leeweiiii@163.com>
2018-10-07 10:26:50 +0800

    car driving

--- hosts.py
+++ hosts.py
@@ -1,5 +1,3 @@
-import string_permutation
-
 def func1(string):
     ln = len(string)
     if ln <= 1:
@@ -12,24 +10,45 @@ def func1(string):
             ret.append(string[i] + string[j])
     return ret;
 
-string = "abcdefghigklmnopqrstuvwxyz"
-a = func1(string)
-for i in xrange(len(a)):
-    a.append(a[i][1] + a[i][0])
+def func2(string):
+    a = func1(string)
+    for i in string:
+        a.append(i + i)
+    return a
+
+def func3(string):
+    a = func1(string)
+    for i in xrange(len(a)):
+        a.append(a[i][1] + a[i][0])
+    return a
+    
+alpha = "abcdefghigklmnopqrstuvwxyz"
+num = "0123456789"
+a = func3(alpha)
 aa = []
 for i in a:
     aa.append(i[0] + i[0] + i[1] + i[1])
+
 b = []
-c = "0123456789"
 for i in aa:
-    for j in c:
+    for j in num:
         b.append(i + j + j + ".com")
+
 b.append("qza123.com")
 b.append("www.xvideos.com")
 b.append("ero-video.com")
 b.append("ero-video.net")
-b.append("dgbyg55.com")
-b.append("dgbyg54.com")
-b.append("dgbyg53.com")
+
+a = func2(num)
+for i in xrange(len(a)):
+    if a[i][1] == a[i][0]:
+        continue
+    a.append(a[i][1] + a[i][0])
+
+for i in a:
+    b.append("dyby" + i + ".com")
+    b.append("dgby" + i + ".com")
+    b.append("dgbyg" + i + ".com")
+
 for i in b:
     print "127.0.0.1 " + i
