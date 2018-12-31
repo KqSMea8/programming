beca26ffdfd3a13eb00d2ba8b9aaf0cb02a38798
leewei <leeweiiii@163.com>
2018-12-31 11:37:08 +0800

    /etc/hosts

--- hosts.py
+++ hosts.py
@@ -8,13 +8,15 @@ aabb = []
 for i in ab:
     aabb.append(i[0] + i[0] + i[1] + i[1])
 
-two_digit_list = [i for i in itertools.product(num, repeat = 2)]
-two_digit_list = [i[0] + i[1] for i in two_digit_list]
+#two_digit_list = [i for i in itertools.product(num, repeat = 2)]
+#two_digit_list = [i[0] + i[1] for i in two_digit_list]
 
 sites = set()
 for i in aabb:
-    for j in two_digit_list:
-        sites.add(i + j + ".com")
+#for j in two_digit_list:
+#sites.add(i + j + ".com")
+    for j in num:
+        sites.add(i + j * 2 + ".com")
 
 sites.add("qza123.com")
 sites.add("www.xvideos.com")
@@ -28,10 +30,12 @@ sites.add("www.aikanfuli.info")
 sites.add("weibo.com")
 sites.add("www.weibo.com")
 sites.add("weibo.cn")
+sites.add("dgbyg103.com")
+sites.add("dgbyg104.com")
 sites.add("www.weibo.cn")
 
 n_length_digit_list = []
-max_len = 5 # configible
+max_len = 2 # configible
 for i in range(1, max_len + 1):
     temp = []
     for j in itertools.product(num, repeat = i):
